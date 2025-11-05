c4135cee01dbb9c73c4cfcb09cf88be534344b6f
ChelseaVPutri <chelseaputri563@gmail.com>
2025-11-05 20:19:34 +0700

    memperbaiki overflow

--- resources/views/livewire/kepegawaian-umum/agenda-pejabat.blade.php
+++ resources/views/livewire/kepegawaian-umum/agenda-pejabat.blade.php
@@ -1,6 +1,6 @@
-<div>
+<div class="flex flex-col min-h-screen">
     
-    <div class="mx-auto px-4 sm:px-6 lg:px-8 py-4">
+    <div class="mx-auto px-4 sm:px-6 lg:px-8 py-4 overflow-y-auto flex-grow">
         {{-- Card - Start --}}
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 my-4">
             {{-- Total Pejabat - Start --}}
@@ -87,62 +87,67 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="bg-white border-b-1 border-gray-200">
-                            <td class="px-6 py-4">
-                                1
-                            </td>
-                            <td class="px-6 py-4">
-                                Prof. Dr. Komarudin, M. Si.
-                            </td>
-                            <td class="px-6 py-4">
-                                123456789012345
-                            </td>
-                            <td class="px-6 py-4">
-                                Fakultas Ilmu Sosial dan Hukum
-                            </td>
-                            <td class="px-6 py-4">
-                                <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md">Ada Ditempat</span>
-                            </td>
-                            <td class="px-6 py-4">
-                                <button type="button" data-modal-target="detail-agenda-pejabat" data-modal-toggle="detail-agenda-pejabat" class="text-white bg-unj hover:bg-unj/90 active:bg-unj/80 w-[38px] h-[38px] p-0 flex items-center justify-center rounded-[10px]">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                        class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                    </svg>
-                                </button>
-                            </td>
-                        </tr>
+                        @for ($i = 1; $i < 5; $i++)
+                            <tr class="bg-white border-b-1 border-gray-200">
+                                <td class="px-6 py-4">
+                                    {{ $i }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    Prof. Dr. Komarudin, M. Si.
+                                </td>
+                                <td class="px-6 py-4">
+                                    123456789012345
+                                </td>
+                                <td class="px-6 py-4">
+                                    Fakultas Ilmu Sosial dan Hukum
+                                </td>
+                                <td class="px-6 py-4">
+                                    <span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md">Ada Ditempat</span>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <button type="button" data-modal-target="detail-agenda-pejabat" data-modal-toggle="detail-agenda-pejabat"
+                                        class="text-white bg-unj hover:bg-unj/90 active:bg-unj/80 w-[38px] h-[38px] p-0 flex items-center justify-center rounded-[10px]">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                            stroke="currentColor" class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        </svg>
+                                    </button>
+                                </td>
+                            </tr>
+                        @endfor
 
-                        <tr class="bg-white border-b-1 border-gray-200">
-                            <td class="px-6 py-4">
-                                2
-                            </td>
-                            <td class="px-6 py-4">
-                                Prof. Dr. Komarudin, M. Si.
-                            </td>
-                            <td class="px-6 py-4">
-                                123456789012345
-                            </td>
-                            <td class="px-6 py-4">
-                                Fakultas Ilmu Sosial dan Hukum
-                            </td>
-                            <td class="px-6 py-4">
-                                <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md">Sedang Rapat</span>
-                            </td>
-                            <td class="px-6 py-4">
-                                <button type="button"
-                                    class="text-white bg-unj hover:bg-unj/90 active:bg-unj/80 w-[38px] h-[38px] p-0 flex items-center justify-center rounded-[10px]">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                        class="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                            d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                    </svg>
-                                </button>
-                            </td>
-                        </tr>
+                        @for ($i = 5; $i < 6; $i++)
+                            <tr class="bg-white border-b-1 border-gray-200">
+                                <td class="px-6 py-4">
+                                    {{ $i }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    Prof. Dr. Komarudin, M. Si.
+                                </td>
+                                <td class="px-6 py-4">
+                                    123456789012345
+                                </td>
+                                <td class="px-6 py-4">
+                                    Fakultas Ilmu Sosial dan Hukum
+                                </td>
+                                <td class="px-6 py-4">
+                                    <span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-md">Sedang Rapat</span>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <button type="button" data-modal-target="detail-agenda-pejabat" data-modal-toggle="detail-agenda-pejabat"
+                                        class="text-white bg-unj hover:bg-unj/90 active:bg-unj/80 w-[38px] h-[38px] p-0 flex items-center justify-center rounded-[10px]">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                            stroke="currentColor" class="size-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        </svg>
+                                    </button>
+                                </td>
+                            </tr>
+                        @endfor
                     </tbody>
                 </table>
             </div>
