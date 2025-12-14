13b296454e74bd267e3e20ef3ff7e2713dff0468
ChelseaVPutri <chelseavp2706@gmail.com>
2025-12-14 16:31:16 +0700

    feat(kepegawaian-dan-administrasi-umum): membuat tabel tendik dan sinkronisasi data tendik

---
+++ database/migrations/2025_12_13_230800_create_tendik_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tendik', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama')->index();
+            $table->string('nip')->index();
+            $table->unique(['nama', 'nip']);
+            $table->string('unit_kerja');
+            $table->string('gelar_depan')->nullable();
+            $table->string('gelar_belakang')->nullable();
+            $table->string('status_kepegawaian'); // di sipeg namanya cabang
+            $table->string('golongan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tendik');
+    }
+};
