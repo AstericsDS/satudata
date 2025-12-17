89e68045bbbbe53db510bffcf6f0f8a58f621396
AstericsDS <andhika2312@gmail.com>
2025-12-17 17:00:55 +0700

    feat(admin): SSO Authentication

---
+++ app/Http/Controllers/SSOController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class SSOController extends Controller
+{
+    public function redirectToProvider()
+    {
+        $clientId = config('sso.client_id');
+        $loginUrl = config('sso.login_url');
+
+        $response = Http::post($loginUrl, [
+            'client_id' => $clientId
+        ]);
+
+        if ($response->failed()) {
+            return redirect(route('login'))->with('error', 'Gagal terhubung ke server SSO');
+        }
+
+        $data = $response->json();
+
+        $publicKey = $data['data']['public_key'] ?? null;
+        $privateKey = $data['data']['private_key'] ?? null;
+        if (!$publicKey || !$privateKey) {
+            return redirect(route('login'))->with('error', 'Respons tidak valid dari server SSO');
+        }
+        session(['sso_private_key' => $privateKey]);
+
+        $ssoRedirectUrl = config('sso.redirect_url') . '?public_key=' . $publicKey;
+        return redirect()->away($ssoRedirectUrl);
+    }
+
+    public function handleProviderCallback(Request $request)
+    {
+        $privateKey = session('sso_private_key');
+        $jwt = $request->query('token');
+
+        if (!$privateKey || !$jwt) {
+            return redirect(route('login'))->with('error', 'Autentikasi SSO gagal. Sesi tidak valid');
+        }
+
+        try {
+            $decoded = JWT::decode($jwt, new Key($privateKey, 'HS256'));
+        } catch (\Exception $e) {
+            return redirect(route('login'))->with('error', 'Token SSO tidak valid. Silakan coba lagi');
+        }
+        $user = User::where('email', $decoded->email)->first();
+        if ($user) {
+            $userCreate = User::updateOrCreate(
+                ['email' => $decoded->email],
+                [
+                    'name' => $decoded->name,
+                    'password' => bcrypt($decoded->id),
+                ]
+            );
+
+            Auth::login($userCreate, true);
+
+            $request->session()->forget('sso_private_key');
+            $request->session()->regenerate();
+
+            return redirect()->intended(route('sinkronisasi-publik'));
+        } else {
+            return redirect()->intended(route('login'))->with('error', 'Kamu tidak memiliki akses');
+        }
+    }
+}
--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,23 +1,19 @@
-<div class="py-12 px-6 bg-white/80 backdrop-blur-md shadow-2xl rounded-4xl w-[350px] h-[500px] sm:h-[500px] sm:w-[500px] md:w-[600px] transition-all flex flex-col">
-    <div class="flex flex-col items-center gap-2 mb-6">
+<div
+    class="py-12 px-6 bg-white/80 backdrop-blur-md shadow-2xl rounded-4xl w-[350px] h-[500px] sm:h-[500px] sm:w-[500px] md:w-[600px] transition-all flex flex-col justify-center space-y-8">
+    <div class="flex flex-col items-center gap-2">
         <img src="{{asset('assets/images/unj.png')}}" alt="Logo UNJ" class="w-[100px] sm:w-[150px]">
-        <h1 class="mt-2 font-semibold text-2xl sm:text-3xl">DASHBOARD LOGIN</h1>
+        <h1 class="mt-2 font-semibold text-2xl sm:text-3xl">dd</h1>
     </div>
-    <form action="" class="flex flex-col gap-4 items-center px-6 h-full mt-8">
-        <div class="relative flex items-center">
-            <span class="material-symbols-rounded absolute left-3">
-                person
-            </span>
-            <input type="text" class="rounded-md  border-2 p-[5px] pl-10 focus:outline-none w-[250px] sm:w-[300px]" placeholder="Username">
+    @if(session('error'))
+        <div class="p-3 rounded-md bg-red-200 text-red-500 my-4 w-fit mx-auto text-center">
+            <span>{{ session('error') }}</span>
         </div>
-        <div class="relative flex items-center">
-            <span class="material-symbols-rounded absolute left-2">
-                lock
-            </span>
-            <input type="password" class="rounded-md  border-2 p-[5px] pl-10 focus:outline-none w-[250px] sm:w-[300px]" placeholder="Password">
+    @endif
+    <a href="{{ route('sso.login') }}">
+        <div
+            class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 w-[75%] mx-auto flex space-x-2 items-center transition-all justify-center shadow-lg">
+            <img src="{{ asset('assets/login/microsoft.svg') }}" alt="Microsoft" class="w-8">
+            <span>Login dengan SSO</span>
         </div>
-        <button type="submit" class="mt-4 mb-4 bg-[#006569] rounded-md cursor-pointer transition-all h-[40px] hover:opacity-80 w-[250px] sm:w-[300px]">
-            <span class="text-white font-semibold">LOGIN</span>
-        </button>
-    </form>
+    </a>
 </div>
