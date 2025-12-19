f03d2fa8e3d297b3f77cf3e90bf977b9036ef582
AstericsDS <andhika2312@gmail.com>
2025-12-19 10:33:41 +0700

    fix(main): Minor fix

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -2,7 +2,7 @@
     class="py-12 px-6 bg-white/80 backdrop-blur-md shadow-2xl rounded-4xl w-[350px] h-[500px] sm:h-[500px] sm:w-[500px] md:w-[600px] transition-all flex flex-col justify-center space-y-8">
     <div class="flex flex-col items-center gap-2">
         <img src="{{asset('assets/images/unj.png')}}" alt="Logo UNJ" class="w-[100px] sm:w-[150px]">
-        <h1 class="mt-2 font-semibold text-2xl sm:text-3xl">dd</h1>
+        <h1 class="mt-2 font-semibold text-2xl sm:text-3xl">DASHBOARD LOGIN</h1>
     </div>
     @if(session('error'))
         <div class="p-3 rounded-md bg-red-200 text-red-500 my-4 w-fit mx-auto text-center">
