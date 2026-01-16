b36abb7cfe85fb3768c39482a9da6d78fe0cde76
ChelseaVPutri <chelseaputri563@gmail.com>
2026-01-17 00:16:54 +0700

    style(main/dashboard): update responsive (hide next and prev button)

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -127,9 +127,9 @@
             class="group relative bg-linear-to-b from-primary from-20% to-accent-2 w-[95%] md:w-[90%] xl:w-[65%] mx-auto rounded-md flex flex-col gap-4 px-4 pb-8 justify-center"
         >
 
-            {{-- Change Button --}}
+            {{-- Previous Button --}}
             <button 
-                class="absolute top-[50%] -left-14 text-primary opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer"
+                class="hidden md:block absolute top-[50%] -left-14 text-primary opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer"
                 @click="active = (active >= 0 && active <= 3) ? 4 : 0"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
@@ -138,9 +138,11 @@
                 </svg>
             </button>
 
-            {{-- Change Button --}}
-            <button @click="active = (active >= 0 && active <= 3) ? 4 : 0"
-                class="absolute top-[50%] -right-14 text-primary opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer">
+            {{-- Next Button --}}
+            <button
+                class="hidden md:block absolute top-[50%] -right-14 text-primary opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer"
+                @click="active = (active >= 0 && active <= 3) ? 4 : 0"
+            >
                 <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 24 24">
                     <path fill="currentColor"
                         d="m12.2 13l-.9.9q-.275.275-.275.7t.275.7t.7.275t.7-.275l2.6-2.6q.3-.3.3-.7t-.3-.7l-2.6-2.6q-.275-.275-.7-.275t-.7.275t-.275.7t.275.7l.9.9H9q-.425 0-.712.288T8 12t.288.713T9 13zm-.2 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0-8" />
