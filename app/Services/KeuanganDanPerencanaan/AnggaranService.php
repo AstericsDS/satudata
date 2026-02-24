7934802ee6befbc267bb09da5824b420836c042a
ChelseaVPutri <chelseaputri563@gmail.com>
2026-02-24 15:33:53 +0700

    refactor(keuangan-dan-perencanaan): fix bug sinkron by satuan kerja dan filter tahun

--- app/Services/KeuanganDanPerencanaan/AnggaranService.php
+++ app/Services/KeuanganDanPerencanaan/AnggaranService.php
@@ -13,52 +13,17 @@ use Illuminate\Support\Facades\Cache;
 class AnggaranService
 {
     private $sync;
-    // private $year;
 
     public function __construct()
     {
         $this->sync = Synchronize::where('name', 'Anggaran dan Daya Serap')->first();
-        // $this->year = now()->year;
     }
+
     public function getTotal($year)
     {
-        // $current_year = now()->year;
-        // $range_year = range($current_year, $current_year - 4);
-        // foreach($range_year as $year) {
-        //     $response = Http::withToken(config('api.saku_token'))->get(config('api.saku_base_url') . '/v2/statistik/realisasi-pagu?thang=' . $year . '&kd_satker=999');
-        //     $data = $response->json();
-
-        //     if (!isset($data['data']) || (isset($data['code']) && $data['code'] !== 0)) {
-        //         throw new Exception('Invalid API Response: ' . json_encode($data));
-        //     }
-
-        //     $item = $data['data'];
-
-        //     $rows = [
-        //         'satker' => 'Universitas Negeri Jakarta',
-        //         'tahun' => $year,
-        //         'data_scope' => 'total',
-        //         'nama' => 'Total Anggaran',
-        //         'pagu_total' => $item['pagu_total'],
-        //         'pagu_realisasi' => $item['pagu_realisasi'],
-        //         'pagu_sisa' => $item['pagu_sisa'],
-        //         'created_at' => now(),
-        //         'updated_at' => now(),
-        //     ];
-
-        //     DB::table('anggaran')->upsert(
-        //         $rows,
-        //         ['tahun', 'satker', 'nama', 'data_scope'],
-        //         ['pagu_total', 'pagu_realisasi', 'pagu_sisa']
-        //     );
-
-        // }
-        // dd($data);
 
         $response = Http::withToken(config('api.saku_token'))->get(config('api.saku_base_url') . '/v2/statistik/realisasi-pagu?thang=' . $year . '&kd_satker=999');
         $data = $response->json();
-
-        // dd($data);
 
         if (!isset($data['data']) || (isset($data['code']) && $data['code'] !== 0)) {
             throw new Exception('Invalid API Response: ' . json_encode($data));
@@ -84,12 +49,10 @@ class AnggaranService
             ['pagu_total', 'pagu_realisasi', 'pagu_sisa']
         );
 
-        // $this->sync->update(['status' => 'synchronized']);
-
     }
 
     public function getPerSatker($year) {
-        $response_satker = Http::withToken(config('api.saku_token'))->get(config('api.saku_base_url') . '/referensi/satker');
+        $response_satker = Http::acceptJson()->withToken(config('api.saku_token'))->get(config('api.saku_base_url') . '/referensi/satker');
         $data_satker = $response_satker->json();
 
         if (!isset($data_satker['data'])) {
@@ -97,14 +60,9 @@ class AnggaranService
         }
 
         $satker_list = $data_satker['data'];
-
         $rows = [];
 
         foreach ($satker_list as $satker) {
-            // $kd_satker = $satker['kd_satker'];
-
-            // $response = Http::withToken(config('api.saku_token'))->get(config('api.saku_base_url') . '/statistik/realisasi-pagu?thang=' . $year . '&kd_satker=' . $kd_satker);
-            // $data = $response->json();
             try {
                 if (empty($satker['kd_satker'])) {
                     continue;
@@ -112,7 +70,7 @@ class AnggaranService
 
                 $kd_satker = $satker['kd_satker'];
 
-                $response = Http::withToken(config('api.saku_token'))->get(config('api.saku_base_url') . '/statistik/realisasi-pagu?thang=' . $year . '&kd_satker=' . $kd_satker);
+                $response = Http::acceptJson()->withToken(config('api.saku_token'))->get(config('api.saku_base_url') . '/statistik/realisasi-pagu?thang=' . $year . '&kd_satker=' . $kd_satker);
                 $data = $response->json();
 
                 if (isset($data['data']) && $data['code'] === 0) {
@@ -120,7 +78,7 @@ class AnggaranService
 
                     $rows[] = [
                         'satker' => $satker['ur_satker'],
-                        'tahun' => $item['thang'],
+                        'tahun' => $year,
                         'data_scope' => 'total',
                         'nama' => 'Total Anggaran',
                         'pagu_total' => $item['pagu_total'],
@@ -129,18 +87,12 @@ class AnggaranService
                         'created_at' => now(),
                         'updated_at' => now(),
                     ];
-                }
+                } 
+                usleep(800000);
             } catch (Exception $error) {
                 Log::warning("Failed to fetch satker {$satker['kd_satker']} for year {$year}: " . $error->getMessage());
                 continue;
             }
-
-            // if (isset($data['code']) && $data['code'] === 0) {
-            //     dump("BERHASIL: Data ditemukan untuk Satker " . $kd_satker . " (" . $satker['ur_satker'] . ")");
-            //     dump($data);
-            // } else {
-            //     dump("KOSONG: Tidak ada data untuk Satker " . $kd_satker);
-            // }
         }
 
         DB::table('anggaran')->upsert(
@@ -148,14 +100,6 @@ class AnggaranService
             ['tahun', 'satker', 'nama', 'data_scope'],
             ['pagu_total', 'pagu_realisasi', 'pagu_sisa', 'updated_at']
         );        
-
-        // if (!empty($rows)) {
-        //     DB::table('anggaran')->upsert(
-        //         $rows,
-        //         ['tahun', 'satker', 'nama', 'data_scope'],
-        //         ['pagu_total', 'pagu_realisasi', 'pagu_sisa', 'updated_at']
-        //     );
-        // }
     }
 
     public function getAkun($year)
@@ -229,17 +173,23 @@ class AnggaranService
 
     public function synchronize()
     {
+        set_time_limit(0);
+        ini_set('memory_limit', '2048M');
         try {
             $current_year = now()->year;
             $range_year = range($current_year, $current_year - 2);
-            // $response_satker = Http::withToken(config('api.saku_token'))->get(config('api.saku_base_url') . '/referensi/satker');
-            // $satker_list = $response_satker->json('data');
-
+        
             foreach($range_year as $year) {
-                // $this->getTotal($year);
-                $this->getPerSatker($year);
-            //     $this->getAkun($year);
-            //     $this->getOuput($year);
+                try {
+                    $this->getTotal($year);
+                    $this->getPerSatker($year);
+                    $this->getAkun($year);
+                    $this->getOuput($year);
+                } catch (Exception $error) {
+                    Log::error("Gagal sync data untuk tahun {$year}: " . $error->getMessage());
+                    continue;
+                }
+                
             }
 
             if ($this->sync) {
